03244dffe77e8816b25071f2dd7a3f65607a5c74
junior_yoomi <junior.chimene@yoomi-solution.com>
2025-02-20 09:23:13 +0100

    Add new feature

--- back/server.js
+++ back/server.js
@@ -5,8 +5,9 @@ prisma.$connect().then(async () => {
     console.log('Connected to the database')
 
     app.listen(3000, '0.0.0.0', () => {
-        console.log('Server running on port 3000')
+        console.log('Server running on port 3000, http://localhost:3000')
     })
 }).catch((error) => {
     console.error(error)
 })
+
